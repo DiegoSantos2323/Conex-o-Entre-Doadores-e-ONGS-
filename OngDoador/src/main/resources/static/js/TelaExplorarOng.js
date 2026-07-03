767caa571419d3839595dae25e254e171b300337
Acesso Livre <Acesso Livre@BC7N278824>
2026-07-03 20:23:12 -0300

    Nova Tela Doacao

--- OngDoador/src/main/resources/static/js/TelaExplorarOng.js
+++ OngDoador/src/main/resources/static/js/TelaExplorarOng.js
@@ -1,44 +1,51 @@
 const API_BUSCAR_NOME_ONG = "http://localhost:8000/ong/buscarnome";
-const API_LISTAR_CAMPANHAS_ONG = "http://localhost:8000/campanha/listarporong";
-const API_LISTAR_ONGS = "http://localhost:8000/ong/listartodos";
+const API_LISTAR_CAMPANHAS = "http://localhost:8000/campanha/listartodos";
 
 
-async function carregarOngs() {
+async function carregarCampanhas() {
 
-    const response = await fetch(API_LISTAR_ONGS);
-
+    const response = await fetch(API_LISTAR_CAMPANHAS);
     if (!response.ok) {
-        alert("Erro ao carregar as ONGs.");
+        alert("Erro ao carregar campanhas.");
         return;
     }
-
-    const ongs = await response.json();
+    const campanhas = await response.json();
     const container = document.getElementById("ongGrid");
-
     container.innerHTML = "";
-    ongs.forEach(ong => {
+    campanhas.forEach(campanha => {
+		
         container.innerHTML += `
             <div class="ong-card">
                 <div class="card-img">
-                    <img src="${ong.logo}" alt="${ong.nomeFantasia}">
-                    <span class="area-badge">${ong.areaAtuacao}</span>
+                    <img src="${campanha.ong.logo}" alt="${campanha.ong.nomeFantasia}">
+                    <span class="area-badge">${campanha.ong.areaAtuacao ?? ""}</span>
                 </div>
-				
                 <div class="card-body">
-                    <h3>${ong.nomeFantasia}</h3>
+                    <h3>${campanha.nomeCampanha}</h3>
                     <p class="card-desc">
-                        ${ong.descricao}
+                        ONG: ${campanha.ong.nomeFantasia}
                     </p>
                     <div class="card-footer">
-                        <a href="TelaOng.html?id=${ong.id}"
+                        <div class="progress-labels">
+                            <span>R$ ${campanha.arrecadado}</span>
+                            <span class="muted">de R$ ${campanha.meta}</span>
+                        </div>
+                        <div class="progress-bar">
+                            <div class="progress-fill"
+                             style="width:${(campanha.arrecadado / campanha.meta) * 100}%">
+                            </div>
+                        </div>
+                        <a href="TelaDoacao.html?id=${campanha.id}"
                            class="btn-ver">
-                            Ver Detalhes
+                            Ver Detalhes 
                         </a>
                     </div>
+					
                 </div>
             </div>
         `;
+	                                                                              
     });
-}
+}	//ao clicar em ver detalhes direciona pra "href="TelaDoacao.html?id=${campanha.id}"";       
+window.onload = carregarCampanhas;
 
-window.onload = carregarOngs;
